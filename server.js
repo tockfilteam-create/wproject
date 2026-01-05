2a53ed4f7f7984d76ea21afaabf23ae5ce6be945
tockfilteam-create <tockfil.team@mail.ru>
2026-01-05 10:57:07 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -128,3 +128,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("SERVER STARTED ON PORT", PORT);
 });
+app.get("/", (req, res) => {
+  res.send("SERVER WORKS");
+});
